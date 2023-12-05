0e7c319e4966deec832bf8daea6a3b0fdb1db65e
tt <152896246+ttddww@users.noreply.github.com>
2023-12-05 00:08:29 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+script.js
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
